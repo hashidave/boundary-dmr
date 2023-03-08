89396983100047dffbcd4ad47481a168245f005c
david randolph <david.randolph@hashicorp.com>
2023-03-08 10:23:01 -0600

    Turned some hard-coded items to vars

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ provider "hcp" {}
 
 # My HCP Boundary instance
 provider "boundary" {
-  addr                            = "https://868bdfc7-61c6-4f31-b3a0-7bae78941aa0.boundary.hashicorp.cloud"
+  addr                            = var.boundary_addr
   auth_method_id                  = var.boundary_auth_method_id
-  password_auth_method_login_name = "tf-workspace"
+  password_auth_method_login_name = var.boundary_login_name
   password_auth_method_password   = var.TF_WORKSPACE_PWD
 }
 
